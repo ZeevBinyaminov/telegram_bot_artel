5aecde04045123ae0fa1c8b03cd91957009bafcf
ZeevBinyaminov <zzbinyaminov@outlook.com>
2022-10-22 14:35:03 +0300

    done

--- handlers/admin.py
+++ handlers/admin.py
@@ -7,33 +7,7 @@ from database import sqlite_db
 
 
 # check mindmap
-class FSMAdmin(StatesGroup):
-    first = State()
-    second = State()
-    third = State()
-
-
-async def admin_fsm_start(message: types.Message):
-    await FSMAdmin.first.set()
-    await message.reply('FSM запущена\nВведи текст к первому')
-
-
-async def get_first(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['user_tag'] = message.from_user.username
-        data['first_text'] = message.text
-    await FSMAdmin.third.set()
-    await message.reply(f"Текст к первому: {message.text}")
-    await message.reply("теперь введи текст к третьему")
-
-
-async def get_third(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['third_text'] = message.text
-    await message.reply(f"Текст к третьему: {message.text}\nFSM завершила работу")
-    # await sqlite_db.sql_add_command(state=state, table_name='test')
-    await state.finish()
-
+# class FSMAdmin(StatesGroup):
 
 async def cancel_handler(message: types.Message, state=FSMContext):
     current_state = await state.get_state()
@@ -51,10 +25,7 @@ async def sql_select_command(message: types.Message):
 def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(cancel_handler, state="*", commands='отмена')
     dp.register_message_handler(cancel_handler, Text(equals='отмена', ignore_case=True), state="*")
-    dp.register_message_handler(admin_fsm_start, commands=['checkfsm'], state=None)
-    dp.register_message_handler(get_first, state=FSMAdmin.first)
-    dp.register_message_handler(get_third, state=FSMAdmin.third)
-    #
+
     dp.register_message_handler(sql_select_command, commands=['получить'], state=None)
     dp.register_message_handler(sql_select_command,
                                 Text(contains='получить', ignore_case=True),
